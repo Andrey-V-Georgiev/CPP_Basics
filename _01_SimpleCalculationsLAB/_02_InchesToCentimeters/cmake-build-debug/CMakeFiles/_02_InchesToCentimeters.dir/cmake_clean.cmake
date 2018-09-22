4f13fc62d0ee72f1ef19059e1a32f428ca10cdc4
Andro <andrey.v.georgiev@gmail.com>
2018-09-22 16:12:26 +0300

    _03_GreetingByName

---
+++ _01_SimpleCalculationsLAB/_02_InchesToCentimeters/cmake-build-debug/CMakeFiles/_02_InchesToCentimeters.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_02_InchesToCentimeters.dir/main.cpp.obj"
+  "_02_InchesToCentimeters.pdb"
+  "_02_InchesToCentimeters.exe"
+  "_02_InchesToCentimeters.exe.manifest"
+  "lib_02_InchesToCentimeters.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_02_InchesToCentimeters.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
