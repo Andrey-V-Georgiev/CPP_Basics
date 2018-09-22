f718201717ab4be06a589b7e08dda2c7c19c44b2
Andro <andrey.v.georgiev@gmail.com>
2018-09-22 15:40:00 +0300

    _02_InchesToCentimeters

---
+++ _01_SimpleCalculationsLAB/_01_RectangleArea/cmake-build-debug/CMakeFiles/_01_RectangleArea.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_01_RectangleArea.dir/main.cpp.obj"
+  "_01_RectangleArea.pdb"
+  "_01_RectangleArea.exe"
+  "_01_RectangleArea.exe.manifest"
+  "lib_01_RectangleArea.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_01_RectangleArea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _01_SimpleCalculationsLAB/_02_InchesToCentimeters/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double inputInches;
+    cin >> inputInches;
+    cout << inputInches * 2.54 << endl;
+
+    return 0;
+}
