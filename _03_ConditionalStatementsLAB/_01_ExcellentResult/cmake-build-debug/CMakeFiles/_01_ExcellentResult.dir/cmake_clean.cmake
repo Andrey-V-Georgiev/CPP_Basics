302c7fbd827f0520b1e4ee786f1197bb9ad00572
root <root@andrey.net1.bg>
2018-10-24 20:08:49 +0300

    _03_ConditionalStatementsLAB

---
+++ _03_ConditionalStatementsLAB/_01_ExcellentResult/cmake-build-debug/CMakeFiles/_01_ExcellentResult.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_01_ExcellentResult.dir/main.cpp.o"
+  "_01_ExcellentResult.pdb"
+  "_01_ExcellentResult"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_01_ExcellentResult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _03_ConditionalStatementsLAB/_01_ExcellentResult/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    double input; std::cin >> input;
+
+    if(input >= 5.50 && input <= 6.00) {
+        std::cout << "Excellent!" << "\n";
+    }
+
+    return 0;
+}
