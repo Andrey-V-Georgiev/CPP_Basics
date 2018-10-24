9b519b317d16ff50023703850469b2c97e2c17be
root <root@andrey.net1.bg>
2018-10-24 20:55:52 +0300

    _02_EvenOrOdd

---
+++ _03_ConditionalStatementsLAB/_02_EvenOrOdd/cmake-build-debug/CMakeFiles/_02_EvenOrOdd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_02_EvenOrOdd.dir/main.cpp.o"
+  "_02_EvenOrOdd.pdb"
+  "_02_EvenOrOdd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_02_EvenOrOdd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _03_ConditionalStatementsLAB/_02_EvenOrOdd/cmake-build-debug/CMakeFiles/_02_EvenOrOdd.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for _02_EvenOrOdd.
+# This may be replaced when dependencies are built.
