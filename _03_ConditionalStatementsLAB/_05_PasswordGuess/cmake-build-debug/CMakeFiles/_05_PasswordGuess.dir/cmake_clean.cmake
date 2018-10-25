c4f011c500a942dd9262c8bb668e135e290d9a85
root <root@andrey.net1.bg>
2018-10-25 23:42:06 +0300

    _05_PasswordGuess/

---
+++ _03_ConditionalStatementsLAB/_05_PasswordGuess/cmake-build-debug/CMakeFiles/_05_PasswordGuess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_05_PasswordGuess.dir/main.cpp.o"
+  "_05_PasswordGuess.pdb"
+  "_05_PasswordGuess"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_05_PasswordGuess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _03_ConditionalStatementsLAB/_05_PasswordGuess/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string rightPassword = "s3cr3t!P@ssw0rd";
+    std::string userInput;
+    std::cin >> userInput;
+
+    if (userInput.compare(rightPassword) == 0) {
+        std::cout << "Welcome" << std::endl;
+    } else {
+        std::cout << "Wrong password!" << std::endl;
+    }
+
+    return 0;
+}
