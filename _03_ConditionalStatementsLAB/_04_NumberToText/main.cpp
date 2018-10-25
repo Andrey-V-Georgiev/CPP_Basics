87b0410304e3e15b979dfe6e622f8cc94d30829b
root <andrey.v.georgiev@gmail.com>
2018-10-26 00:20:27 +0300

    _04_NumberToText

--- _03_ConditionalStatementsLAB/_04_NumberToText/main.cpp
+++ _03_ConditionalStatementsLAB/_04_NumberToText/main.cpp
@@ -7,27 +7,27 @@ int main() {
     std::cin >> input;
 
     if (input == 0) {
-        std::cout << "zero" << "\n";
+        std::cout << "zero" << std::endl;
     } else if (input == 1) {
-        std::cout << "one" << "\n";
+        std::cout << "one" << std::endl;
     } else if (input == 2) {
-        std::cout << "two" << "\n";
+        std::cout << "two" << std::endl;
     } else if (input == 3) {
-        std::cout << "three" << "\n";
+        std::cout << "three" << std::endl;
     } else if (input == 4) {
-        std::cout << "four" << "\n";
+        std::cout << "four" << std::endl;
     } else if (input == 5) {
-        std::cout << "five" << "\n";
+        std::cout << "five" << std::endl;
     } else if (input == 6) {
-        std::cout << "six" << "\n";
+        std::cout << "six" << std::endl;
     } else if (input == 7) {
-        std::cout << "seven" << "\n";
+        std::cout << "seven" << std::endl;
     } else if (input == 8) {
-        std::cout << "eight" << "\n";
+        std::cout << "eight" << std::endl;
     } else if (input == 9) {
-        std::cout << "nine" << "\n";
+        std::cout << "nine" << std::endl;
     } else {
-        std::cout << "number too big" << "\n";
+        std::cout << "number too big" << std::endl;
     }
 
 
