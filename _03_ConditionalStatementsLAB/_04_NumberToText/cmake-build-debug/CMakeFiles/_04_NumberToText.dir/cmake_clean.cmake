eb9ee5a721fd8a46c12aef05575826e1402537da
root <root@andrey.net1.bg>
2018-10-25 23:27:44 +0300

    _04_NumberToText

---
+++ _03_ConditionalStatementsLAB/_04_NumberToText/cmake-build-debug/CMakeFiles/_04_NumberToText.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_04_NumberToText.dir/main.cpp.o"
+  "_04_NumberToText.pdb"
+  "_04_NumberToText"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_04_NumberToText.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _03_ConditionalStatementsLAB/_04_NumberToText/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+int main() {
+
+    int input;
+    std::cin >> input;
+
+    if (input == 0) {
+        std::cout << "zero" << "\n";
+    } else if (input == 1) {
+        std::cout << "one" << "\n";
+    } else if (input == 2) {
+        std::cout << "two" << "\n";
+    } else if (input == 3) {
+        std::cout << "three" << "\n";
+    } else if (input == 4) {
+        std::cout << "four" << "\n";
+    } else if (input == 5) {
+        std::cout << "five" << "\n";
+    } else if (input == 6) {
+        std::cout << "six" << "\n";
+    } else if (input == 7) {
+        std::cout << "seven" << "\n";
+    } else if (input == 8) {
+        std::cout << "eight" << "\n";
+    } else if (input == 9) {
+        std::cout << "nine" << "\n";
+    } else {
+        std::cout << "number too big" << "\n";
+    }
+
+
+    return 0;
+}
